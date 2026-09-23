26eac1b57364c06f13232db599d25edceb7bd72d
Guillaume "Vermeille" Sanchez <Guillaume.V.Sanchez@gmail.com>
2026-09-23 15:09:59 +0200

    feat: mount developer workbench from query flag

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import { createApp } from 'vue';
 import './style.css';
-import App from './App.vue';
 
-createApp(App).mount('#app');
+async function mount(): Promise<void> {
+  const devMode = new URLSearchParams(window.location.search).has('dev');
+  const component = devMode
+    ? (await import('./dev/DevApp.vue')).default
+    : (await import('./App.vue')).default;
+
+  createApp(component).mount('#app');
+}
+
+void mount();
